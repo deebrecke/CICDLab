731e339bf15dde1cd16897e46cff521c42bcad06
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-12-08 11:52:01 -0800

    Removed everything except the run method in main to improve CodeCov %

--- src/main/java/com/example/cicdlab/CiCdLabApplication.java
+++ src/main/java/com/example/cicdlab/CiCdLabApplication.java
@@ -10,14 +10,5 @@ public class CiCdLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdLabApplication.class, args);
-        Dice redDie = new Dice(6, "red");
-        Dice greenDie = new Dice(20, "green");
-        int[] rollsArray = redDie.rollMany(5);
-        System.out.println(Arrays.toString(rollsArray));
-
-        int sides = 6;
-        int[] rollsHistory = greenDie.rollMany(10);
-        System.out.println(Arrays.toString(rollsHistory));
     }
-
 }
