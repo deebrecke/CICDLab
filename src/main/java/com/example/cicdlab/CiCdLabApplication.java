d50b8eb0ffa953fc62dc34dd1aae13359727105b
deebrecke <brecke.deirdre@student.greenriver.edu>
2023-12-08 11:33:28 -0800

    Found an error in the rollMany method in Dice class and fixed it.

--- src/main/java/com/example/cicdlab/CiCdLabApplication.java
+++ src/main/java/com/example/cicdlab/CiCdLabApplication.java
@@ -3,11 +3,16 @@ package com.example.cicdlab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CiCdLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdLabApplication.class, args);
+        Dice redDie = new Dice(6, "red");
+        int[] rollsArray = redDie.rollMany(5);
+        System.out.println(Arrays.toString(rollsArray));
     }
 
 }
